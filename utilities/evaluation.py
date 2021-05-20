656632af3a706b4d6c75cb8af60e37d1e8863486
HarrrrisZhang <82919281+HarrrrisZhang@users.noreply.github.com>
2021-05-20 11:41:08 +1000

    evaluation.py

--- utilities/evaluation.py
+++ utilities/evaluation.py
@@ -2,35 +2,20 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
 
-def simple_accuracy(predicted_file):
-    """calls sklearn for simple accuracy calculation on the given file"""
-    
-    predicted = pd.read_csv(predicted_file, header=0)
-    for col in [col for col in predicted.columns if 'prediction' in col]:
-        
-        score = accuracy_score(predicted['duration_label'], predicted[col])
-        print(f"Accuracy for '{col}': {(score*100):.2f}%")
+def evaluate(yhat, y):
+    """format print precision, recall, f1 score & confusion matrix"""
 
+    accuracy = accuracy_score(y, yhat)
+    print(f"Accuracy = {(accuracy*100):.2f}%")
 
-def evaluate(predicted_file):
-    """prints precision & recall"""
-    
-    predicted = pd.read_csv(predicted_file, header=0)
-    for col in [col for col in predicted.columns if 'prediction' in col]:
-        
-        print(f"\nNow analyzing performance of '{col}'\n")
-        
-        accuracy = accuracy_score(predicted['duration_label'], predicted[col])
-        print(f"Accuracy = {(accuracy*100):.2f}%")
-        
-        precision = precision_score(predicted['duration_label'], predicted[col], average=None, zero_division=0)
-        recall = recall_score(predicted['duration_label'], predicted[col], average=None, zero_division=0)
-        f1 = f1_score(predicted['duration_label'], predicted[col], average=None, zero_division=0)
-        
-        score = pd.DataFrame({'Precision':precision, "Recall":recall, "F_score":f1}, index=[1,2,3])
-        print(score)
-        
-        matrix = confusion_matrix(predicted['duration_label'], predicted[col])
-        matrix = pd.DataFrame(matrix, index=[1,2,3], columns=[1,2,3])
-        print("\nConfusion matrix:")
-        print(matrix, end='\n\n')
+    precision = precision_score(y, yhat, average=None, zero_division=0)
+    recall = recall_score(y, yhat, average=None, zero_division=0)
+    f1 = f1_score(y, yhat, average=None, zero_division=0)
+
+    score = pd.DataFrame({'Precision':precision, "Recall":recall, "F_score":f1}, index=[1,2,3])
+    print(score)
+
+    matrix = confusion_matrix(y, yhat)
+    matrix = pd.DataFrame(matrix, index=[1,2,3], columns=[1,2,3])
+    print("\nConfusion matrix:")
+    print(matrix, end='\n\n')
